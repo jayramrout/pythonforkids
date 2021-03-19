497cc13fef1373ed1f115435f4c4fc82a4d5910c
Jayram Rout <jrout@healthedge.com>
2021-03-19 17:54:35 -0400

    Updates

---
+++ 07_Function_Assignment.py
@@ -0,0 +1,72 @@
+# Assignment 1:
+'''
+    Given a text 99 print the below
+    Use a function to build a common function
+    Use zfill() function
+    99
+    099
+    0099
+    00099
+    000099
+    00099
+    0099
+    099
+    99
+'''
+# Assignment 2:
+'''
+    Given an array print all the pairs
+    Note: Use two for loop
+    Eg : given numbers = [1,2,3]
+    output
+    1,1
+    1,2
+    1,3
+    2,1
+    2,2
+    2,3
+    3,1
+    3,2
+    3,3
+'''
+
+# Assignment 3:
+'''
+    Use the assignment 2 , print the sum of each pair using add_function
+    output
+    1,1 = 2
+    1,2 = 3
+    1,3 = 4
+    2,1 = 3
+    2,2 = 4
+    2,3 = 5
+    3,1 = 4
+    3,2 = 5
+    3,3 = 6
+'''
+
+# Assignment 4:
+'''
+    Take an input year from input() function
+
+    Write a function[is_leap_year(year)] to calculate if a year is a leap year and returns YES else NO
+    Pass the input variable to the isLeapYear(year) function which returns YES or NO
+    Write another function which takes the year and the result of isLeapYear() function
+    eg : printMessage(year,result)
+    This function prints the final message as below
+    Final output should be
+      YES You have entered a Leap Year and the year is <Actual Year>
+'''
+# Assignment 5:
+'''
+    Write a function(return_month(month_num)) which returns the name of the Month given a number.
+    Suggestion : inside the return_month use a map of key value pair to store the months instead of using if else. 
+    eg: return_month(3) returns MARCH
+    
+    Write another function (print_more_info()) which takes the month_num and the return value of return_month function
+    and prints the below message:
+    
+        if the number falls in 1st quarter then  
+        print "You are in the 1st quarter of the season" likewise for other months
+        (1st quarter: 1,2,3 --- 2nd quarter:4,5,6 --- 3rd quarter: 7,8,9 --- 4th quarter : 10,11,12) 
+'''
